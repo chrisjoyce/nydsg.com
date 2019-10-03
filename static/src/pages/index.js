4d453cbfdded05b566f69c2bf2bee2f7f7651383
Chris Joyce <ctjoyce@gmail.com>
2019-10-03 08:25:13 -0600

    adds note

--- static/src/pages/index.js
+++ static/src/pages/index.js
@@ -31,8 +31,17 @@ function IndexPage() {
         <p className="leading-normal pb-5">
           This disease is an equal opportunity disease…it affects young and old, male and female, rich and poor. Our members here have all had different experiences with diverticulitis but share one common belief: we can learn so much from each other about living with this disease.
         </p>
-        <p className="text-center text-4xl">
-          <Link to="members">We invite you to contact us.</Link>
+       
+        <p className="text-center text-2xl">
+          <div><span className="text-yellow-800">**</span> We welcome you to join us for a meeting Saturday October 12th</div>
+          <div>from 12 - 1:30pm at the <a target="_blank" href="https://www.google.com/maps/dir//150%20Holbrook%20Rd,%20Holbrook,%20NY%2011741">Sachem Public Library 150 Holbrook Rd, Holbrook, NY 11741 <span className="text-yellow-800">**</span></a></div>
+        </p>
+        <p className="text-center text-2xl">
+        
+        </p>
+
+        <p className="text-center text-4xl pt-10">
+          <Link to="members">Please feel free to contact us.</Link>
         </p>
       </section>
     </Layout>
