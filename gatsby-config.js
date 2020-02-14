a91a4107a7cfcdf7e172188b1734e338fac18bca
Chris Joyce <ctjoyce@gmail.com>
2020-02-14 12:52:07 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       body: {
         p1: "Check out our new blog “How We Began”.",
         p2:
-          "March is Colorectal Cancer Awareness Month. Visit New York Colorectal Cancer Survivorship and Support Group at http://NYCRCSSG.org for more information."
+          ""
       }
     }
   },
